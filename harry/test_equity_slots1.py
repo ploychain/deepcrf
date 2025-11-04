b18ab3fdefdce5b5690168d8cbc645b1074d5eb6
Your Name <you@example.com>
2025-11-04 18:03:57 +0800

    preflop_equity

--- harry/test_equity_slots1.py
+++ harry/test_equity_slots1.py
@@ -114,7 +114,7 @@ def main():
 
     # 固定 seed，确保可复现
     state = pkrs.State.from_seed(
-        n_players=6, button=0, sb=1, bb=2, stake=200.0, seed=42
+        n_players=6, button=0, sb=1, bb=2, stake=200.0, seed=1
     )
 
     # === Preflop ===
